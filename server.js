acfc9a56410fa24eb77c31aaa8b145d8ec8556d3
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-04-22 15:36:42 -0700

    adds mongoose connection

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const blogRoutes = require('./routes/blogRoutes');
 const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + " is mongo not running?"));
+db.on('connected', () => console.log("MongoDB Connected!"));
+db.on('disconnected', () => console.log("MongoDB Disconnected!"));
