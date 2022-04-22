25662d2509b359c0522bb42d6d1104c86315e5f5
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-04-22 15:39:04 -0700

    adds middleware

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const blogRoutes = require('./routes/blogRoutes');
 const app = express();
-
+app.set('view engine', 'ejs');
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
+
 const db = mongoose.connection;
 db.on('error', (err) => console.log(err.message + " is mongo not running?"));
 db.on('connected', () => console.log("MongoDB Connected!"));
 db.on('disconnected', () => console.log("MongoDB Disconnected!"));
+
+//Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('tiny'));
