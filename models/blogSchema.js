e8cbc2d86436c1b19edb69420564aca709baa14b
Javier Delgado <javierdelgado@Javiers-MacBook-Pro.local>
2022-04-26 19:00:48 -0700

    heroku deployment

--- models/blogSchema.js
+++ models/blogSchema.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const blogSchema = new mongoose.Schema({
     title: {type: String, required: true},
     snippet: {type: String, required: true},
-    body: {type: String, required: true}
+    body: {type: String, required: true},
+    img: {type: String, required: true}
 }, {timestamps: true});
 
 module.exports = mongoose.model('Blog', blogSchema);
--- models/blogSeed.js
+++ models/blogSeed.js
@@ -1,18 +1,21 @@
 const blog = [
     {
       title: "Tuna",
-      snippet: "Dummy text",
-      body: "dummy text"
+      snippet: "Missing dog treks 60 miles to her old home",
+      body: "Some ties can’t be broken. This was certainly the case with Cleo, a yellow labrador-mix who went missing earlier this month. Thankfully, Cleo was found, but it was in the unlikeliest of places—her owner’s previous home. When Colton Michael found an unfamiliar dog on his porch, he assumed it belonged to a nearby neighbor. Once he earned Cleo’s trust, Michael took her to have her microchip scanned. The results were astonishing—Cleo belonged to his home’s previous owner! Even after two years, Cleo was still attached to her old house. Despite what they say, you really can go home again.",
+      img: "https://grumbledog.com/blog/wp-content/uploads/2020/08/Inspiring-Dogs-Photo-2.jpg"
     },
     {
-      title: "Bacon",
-      snippet: "It's just a bag of bones.",
-      body: "dummy text"
+      title: "Barry the Saint Bernard",
+      snippet: "A dog hero",
+      body: "Did you know that Saint Bernards were bred by monks in the 1800’s specifically to rescue travelers stranded in the snowy Swiss Alps? One special dog named Barry took his job very seriously, saving 40 people in his lifetime.Even after his death, Barry’s legacy was immortalized by the Swiss monks who continue to keep one dog named Barry in their monastery at all times.",
+      img: "https://grumbledog.com/blog/wp-content/uploads/2020/08/Inspiring-Dogs-Photo-4.jpg"
     },
     {
-      title: "Ralph",
-      snippet: "dummy text",
-      body: "dummy text"
+      title: "Great Dane helps boy with cerebral palsy walk again",
+      snippet: "Big dogs can be the most gentle",
+      body: "Dogs can be the ultimate motivators. This proved to be true for Hunter VanBrocklin, a young boy with cerebral palsy who underwent surgery for hip dysplasia in 2016. When Hunter needed to learn how to walk again, his beloved  Dane, Wendy, rose to the occasion. With a gentle leader harness around her neck, Wendy helped Hunter maintain his balance, making it possible for the boy to enjoy active pursuits—even hiking!",
+      img: "https://grumbledog.com/blog/wp-content/uploads/2020/08/Inspiring-Dogs-Photo-3.jpg" 
     },
   ];
 
